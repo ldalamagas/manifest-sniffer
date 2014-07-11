8bf226fd22d9e0500d473230ae4643f7ec0c92db
ldalamagas <lefteris.dalamagas@gmail.com>
2014-07-11 10:31:44 +0300

    Add directory search feature

--- manifest_sniffer.py
+++ manifest_sniffer.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 
 import argparse
+import glob
+import os
 from zipfile import ZipFile, BadZipfile, LargeZipFile
 
 
+def get_jars(path):
+    pattern = os.path.join(path, "*.jar")
+    return glob.glob(pattern)
+
+
 def get_manifest_value(jar, manifest_property):
-
     manifest_path = "META-INF/MANIFEST.MF"
-
     with ZipFile(jar, "r") as jar:
 
         manifest = jar.read(manifest_path)
@@ -25,7 +30,7 @@ def get_manifest_value(jar, manifest_property):
         if manifest_property in properties.keys():
             return properties[manifest_property]
         else:
-            return None
+            return ""
 
 
 def get_package_names(manifest_line):
@@ -44,54 +49,66 @@ def create_argument_parser():
     parser.add_argument("-i", "--imports", help="Checks for the given package in the Import-Package value")
     parser.add_argument("-e", "--exports", help="Checks for the given package in the Export-Package value")
     parser.add_argument("-f", "--file", help="The jar to search in")
+    parser.add_argument("-d", "--directory", help="Search all the jars in the given directory")
     return parser
 
 
-def main():
-
-    export_package_key = "Export-Package"
+def print_imports(jar):
     import_package_key = "Import-Package"
     import_error_message = "Error while trying to get imported packages"
+    try:
+        imported_packages = get_package_names(get_manifest_value(jar, import_package_key))
+        if arguments.imports in imported_packages:
+            print("%s imports %s" % (jar, arguments.imports))
+    except RuntimeError:
+        print("%s, can I read %s?" % (import_error_message, jar))
+    except KeyError:
+        print("%s, does %s has a MANIFEST.MF?" % (import_error_message, jar))
+    except BadZipfile:
+        print("%s, %s is not a valid zip file!" % (import_error_message, jar))
+    except IOError:
+        print("%s, are you sure %s exists?" % (import_error_message, jar))
+    except LargeZipFile:
+        print("%s, %s requires ZIP64 functionality but it is not enabled." % (import_error_message, jar))
+
+
+def print_exports(jar):
+    export_package_key = "Export-Package"
     export_error_message = "Error while trying to get exported packages"
+    try:
+        exported_packages = get_package_names(get_manifest_value(jar, export_package_key))
+        if arguments.imports in exported_packages:
+            print("%s exports %s" % (jar, arguments.exports))
+    except RuntimeError:
+        print("%s, can I read %s?" % (export_error_message, jar))
+    except KeyError:
+        print("%s, does %s has a MANIFEST.MF?" % (export_error_message, jar))
+    except BadZipfile:
+        print("%s, %s is not a valid zip file!" % (export_error_message, jar))
+    except IOError:
+        print("%s, are you sure %s exists?" % (export_error_message, jar))
+    except LargeZipFile:
+        print("%s, %s requires ZIP64 functionality but it is not enabled." % (export_error_message, jar))
 
-    argument_parser = create_argument_parser()
-    arguments = argument_parser.parse_args()
 
-    if arguments.file is None:
-        argument_parser.print_help()
+def main():
+    if arguments.file is not None:
+        if arguments.imports is not None:
+            print_imports(arguments.file)
 
-    if arguments.imports is not None:
-        try:
-            imported_packages = get_package_names(get_manifest_value(arguments.file, import_package_key))
-            if arguments.imports in imported_packages:
-                print("%s imports %s" % (arguments.file, arguments.imports))
-        except RuntimeError:
-            print("%s, can I read %s?" % (import_error_message, arguments.file))
-        except KeyError:
-            print("%s, does %s has a MANIFEST.MF?" % (import_error_message, arguments.file))
-        except BadZipfile:
-            print("%s, %s is not a valid zip file!" % (import_error_message, arguments.file))
-        except IOError:
-            print("%s, are you sure %s exists?" % (import_error_message, arguments.file))
-        except LargeZipFile:
-            print("%s, %s requires ZIP64 functionality but it is not enabled." % (import_error_message, arguments.file))
+        if arguments.exports is not None:
+            print_exports(arguments.file)
 
-    if arguments.exports is not None:
-        try:
-            exported_packages = get_package_names(get_manifest_value(arguments.file, export_package_key))
-            if arguments.imports in exported_packages:
-                print("%s exports %s" % (arguments.file, arguments.exports))
-        except RuntimeError:
-            print("%s, can I read %s?" % (export_error_message, arguments.file))
-        except KeyError:
-            print("%s, does %s has a MANIFEST.MF?" % (export_error_message, arguments.file))
-        except BadZipfile:
-            print("%s, %s is not a valid zip file!" % (export_error_message, arguments.file))
-        except IOError:
-            print("%s, are you sure %s exists?" % (export_error_message, arguments.file))
-        except LargeZipFile:
-            print("%s, %s requires ZIP64 functionality but it is not enabled." % (export_error_message, arguments.file))
+    if arguments.directory is not None:
+        for jar in get_jars(arguments.directory):
+            if arguments.imports is not None:
+                print_imports(jar)
+
+            if arguments.exports is not None:
+                print_exports(jar)
 
 if __name__ == '__main__':
+    argument_parser = create_argument_parser()
+    arguments = argument_parser.parse_args()
     main()
 
